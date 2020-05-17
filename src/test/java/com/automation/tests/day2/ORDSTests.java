633c211297b5ea53be68f27294b113055baba822
ankarenver <1996ankarjian@gmail.com>
2020-05-17 17:01:11 -0400

    commit

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,6 +1,7 @@
 package com.automation.tests.day2;
 
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +15,62 @@ public class ORDSTests {
     @Test
     @DisplayName("Get list of all employee")
     public void getAllEmployees(){
-
+        //response can be saved in the Response object
+        //prettyPeek() - method that prints response info in nice format
+        //also ths method returns Response object
+        //response contains body, header and status line
+        //body (payload) - contains content that we requested from the web service
+        //header - contains meta data
         Response response = given().baseUri(BASE_URL).
                 when().get("/employees").prettyPeek();
+
+
+        /**
+         * RestAssured request has similar structure to BDD scenarios:
+         * Start building the request part of the test
+         *
+         * given() - used for request setup and authentication
+         * Syntactic sugar,
+         * when() - to specify type of HTTP request: get, put, post, delete, patch, head, etc...
+         * then() - to verify response, perform assertions
+         */
+    }
+
+
+    @Test
+    @DisplayName("Get employee under specific ID")
+    public void  getOneEmployee(){
+
+        // in URL we can specify path and query parameter
+        // path parameters are used to retrieve specific resource: for example 1 employee not all of them
+        // {id} - path variable, that will be replace with a value after comma
+        // after when() we specify HTTP request type/method/verb
+        //The path parameters. E.g. if path is "/book/{hotelId}/{roomNumber}" you can do <code>get("/book/{hotelName}/{roomNumber}", "Hotels R Us", 22);</code>.
+        Response response = given().baseUri(BASE_URL).when().get("/employees/{id}", 100).prettyPeek();
+        // how we verify response? - user assertions
+
+        response.then().statusCode(200); //to verify that status is 200 ok
+
+        int statusCode = response.statusCode(); // to save status code in variable
+
+        Assertions.assertEquals(200,statusCode);
+    }
+
+
+    /**
+     * given base URI = http://3.90.112.152:1000/ords/hr
+     * when user sends get request to "/countries"
+     * then user verifies that status code is 200
+     */
+
+    @Test
+    @DisplayName("Get all countries")
+    public void getAllCountries(){
+        Response response = given().baseUri(BASE_URL).when().get("/countries").prettyPeek();
+        Assertions.assertEquals(200,response.statusCode());
+
+        // second way
+//        given().baseUri(BASE_URL).when().get("/countries").prettyPeek().then().statusCode(200);
 
     }
 }
---
+++ src/test/java/com/automation/tests/day2/SpartanTests.java
@@ -0,0 +1,48 @@
+package com.automation.tests.day2;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class SpartanTests {
+
+    String BASE_URL = "http://3.90.112.152:8000";
+    // URI (Uniform Resource Identifier ) =  URL + URN = http://www.google.com/index.html
+    // URL (Uniform Resource Locator    ) = http://www.google.com
+    // URN (Uniform Resource Name       ) = /index.html
+
+    @Test
+    @DisplayName("Get list of all spartans")  // optional
+    public void getAllSpartans(){
+        //401 - Unauthorized, since we did not provide credentials request failed
+        // how to provide credentials
+        // there are different type of authentication: basic, oauth 1.0, oauth 2.0, api key, bearer token, etc...
+        // spartan app required basic authentication : username and password
+
+        given().auth().basic("admin", "admin").baseUri(BASE_URL).when().get("/api/spartans").prettyPeek().then().statusCode(200);
+    }
+
+    // add new spartan
+
+    @Test
+    @DisplayName("Add new spartan")
+    public void addNewSpartan(){
+        // JSON supports different fata types: string, integer, boolean
+        String body = "{\"gender\": \"Male\", \"name\": \"snake_tiss_tiss\",\"phone\": 99999789999}";
+
+        // instead of string variable we can use external JSON file
+        // user File class to read JSON and pass it into body
+        // provide path to the JSON as a parameter
+        File jsonFile = new File(System.getProperty("user.dir")+"/spartan.json");
+
+        // to create new item, we perform POST request
+        // contentType(ContentType.JSON)  -- to tell web service what kind of media type we send
+
+        given().contentType(ContentType.JSON).auth().basic("admin", "admin")
+                .body(jsonFile).baseUri(BASE_URL).when().post("/api/spartans").prettyPeek().then().statusCode(201);
+    }
+}
